39a101eca4f01c7a1dc5564c3e4c1bab9524b8cf
itwasalwaysyou <112202949+itwasalwaysyou@users.noreply.github.com>
2023-01-26 22:47:31 -0600

    Update DP-O(n).java

--- Contains Duplicate/DP-O(n).java
+++ Contains Duplicate/DP-O(n).java
@@ -4,7 +4,7 @@ class Solution {
         int Max_Sum= nums[0];
         int Curr_Sum=nums[0];
 
-        for(int i=1; i< nums.length;i++){
+        for(int i=1; i< nums.length;i++){ //skip the first element-> aviod the case with only one element list
 
                  Curr_Sum = Math.max(nums[i],Curr_Sum+nums[i]); //if the sum is less then 0? Y-> Delect N-> Keep
                  Max_Sum = Math.max(Curr_Sum,Max_Sum);
